095965ad065c31e5a0a1ed40c22f485752262183
codecatcoffee <codecatcoffee@gmail.com>
2024-03-28 23:52:30 -0500

    domain data

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import tailwind from '@astrojs/tailwind'
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://codecatcoffee.github.io',
-  base: 'resume'
+  site: 'https://resume.ccc.zone'
 })
